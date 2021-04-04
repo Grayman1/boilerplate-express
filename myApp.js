44ee53beb18abeb3e796d6aa8fb306d0e6487ee4
Grayman1 <>
2021-04-04 16:47:48 +0000

    add sendFile code for Challenge #3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-app.get("/", (req, res) => {
+
+// Challenge #2
+/*app.get("/", (req, res) => {
   res.send('Hello Express');
 });
-
+*/
+// Challenge #3
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html' );
+});
 
 
 
