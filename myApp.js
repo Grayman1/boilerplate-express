d26d2a58d9c72020a311a9d7855f9243512d8fb2
Grayman1 <>
2021-04-04 21:50:22 +0000

    Challenge #7 Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+// Challenge #7 Implement a Root-Level Request Logger Middleware
+
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
 console.log("Hello World");
 console.log(process.env.MESSAGE_STYLE)
 // Challenge #2
@@ -17,11 +24,19 @@ app.get("/", (req, res) => {
 */
 app.use("/public",express.static(__dirname + '/public'));
 
+/*
 // Challenge #5
 let message = {"message": "Hello json"};
+
 app.get("/json", (req, res) => {res.json(message)});
+*/
 
-
+// Challenge #6
+let message = {"message": "Hello json"};
+app.get("/json", (req, res) => {
+  if(process.env.MESSAGE_STYLE === 'uppercase') {res.json({"message": "HELLO JSON"})}
+  else {
+  res.json(message)}});
 
 
 
