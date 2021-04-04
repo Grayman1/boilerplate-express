9cfd65022239205b7ecd04fa339bddefb3c50266
Grayman1 <>
2021-04-04 18:44:09 +0000

    Challenge #5 code updates

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
-
+console.log(process.env.MESSAGE_STYLE)
 // Challenge #2
 /*app.get("/", (req, res) => {
   res.send('Hello Express');
@@ -12,10 +12,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html' );
 });
 
+// Challenge #4
+/* app.use(express.static(__dirname + '/public'));
+*/
+app.use("/public",express.static(__dirname + '/public'));
 
-
-
-
+// Challenge #5
+let message = {"message": "Hello json"};
+app.get("/json", (req, res) => {res.json(message)});
 
 
 
